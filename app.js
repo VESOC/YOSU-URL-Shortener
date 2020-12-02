c28065869bf6cce9e52b0ecca8e874aaf407e691
Coder38611 <54740236+coder38611@users.noreply.github.com>
2020-12-02 18:53:12 +0900

    Updated app.js

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.post('/shorten', async(req, res) => {
     
         let url = req.body.URL
         if (!(url.startsWith('https://') || url.startsWith('http://'))) {
-            url = 'https://' + url
+            url = 'http://' + url
         }
     
         await Link.create({
